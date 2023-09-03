ccd147d549214c0a3c77b41ed70daf1f2bb589c8
alphagutter <142161508+alphagutter@users.noreply.github.com>
2023-09-03 16:03:59 -0400

    Add files via upload

--- PParcial_IsaacOrtega_clases.py
+++ PParcial_IsaacOrtega_clases.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 class Accionable:
+    '''pequeña clase abstracta, donde definimos 2 clases a utilizar en distintos tipos, por el que ocurre el simple polimorfismo'''
     @abstractmethod
     def jugar():
         pass
@@ -10,18 +11,20 @@ class Accionable:
         pass
 
 class Empleado(Accionable):
-    def __init__(self, nombre, barra_cansancio):
-        self.nombre = nombre
-        self.barra_cansancio = barra_cansancio
+    '''Es la clase abstracta con la que definimos los empleados del local'''
     
     @abstractmethod
     def limpiar():
         pass
 
+
+
+
 class Cajero(Empleado):
     '''Es el empleado con el que interactuan los clientes, pueden limpiar, atender a los clientes, entre otras cosas'''
     def __init__(self, nombre, barra_cansancio):
-        super().__init__(nombre, barra_cansancio)
+        self.nombre = nombre
+        self.barra_cansancio = barra_cansancio
 
     #implementar mejor con el objeto sofa
     def descansar(self):
@@ -42,12 +45,18 @@ class Cajero(Empleado):
     def atender_fila(self):
         pass
 
+    def jugar(self):
+        '''La barra de cansancio disminuye cuando el cajero juega'''
+        self.barra_cansancio -= 1
+
+
 
 class Cocinero(Empleado):
     '''Es el empleado encargado de cocinar para los clientes, reciben los pedidos que les pasan los cajeros, de acuerdo a su nivel de cansancio, es más rentable o no que cocinen ciertas comidas de seguido'''
-    def __init__(self, nombre, barra_cansancio, pedidos):
-        super().__init__(nombre, barra_cansancio)
-        self.pedidos = pedidos    #implementar una lista enlazada
+    def __init__(self, nombre, barra_cansancio):
+        self.nombre = nombre
+        self.barra_cansancio = barra_cansancio
+        self.pedidos = []    #implementar una lista enlazada
 
     #seguir implementando
     def cocinar(self):
@@ -72,8 +81,11 @@ class Cocinero(Empleado):
     def entregar_comida(self):
         pass
 
+
+
+
 class Cliente(Accionable):
-    ''''''
+    '''Los clientes son el objeto que más interactúa con otros objetos, sus estados cuando se crean son randomizados'''
     def __init__(self, nombre, entusiasmo, estado_animo, hambre):
         self.nombre = nombre
         self.entusiasmo = entusiasmo
@@ -86,25 +98,59 @@ class Cliente(Accionable):
 
     #implementar con el objeto sofa
     def descansar(self):
-        pass
+        if self.estado_animo <=3:
+            self.estado_animo += 2
+        elif self.estado_animo >=3 and self.estado_animo<6:
+            self.estado_animo += 1
+        else:
+            print('Está eufórico, no descansará aunque le obliguen')
 
-    def jugar(self):
-        pass
+    #implementar con el objeto consola
+    def jugar(self, Consola):
+            if Consola.disponible == False:
+                print('Esa consola no está disponible')
+            else:
+                if self.estado_animo < 3:
+                    print('El cliente no jugará ni aunque le obliguen')
+                elif self.estado_animo >= 3 and self.estado_animo < 6:
+                    print(f'El cliente {self.nombre} está jugando a la consola {Consola}')
+                    self.entusiasmo += 1
+                else:
+                    self.entusiasmo += 3
 
+    #implementar con el objeto comida y objeto mesa
     def comer(self):
         pass
 
+    #implementar con 
     def entrar_fila(self):
         pass
 
+
+
+
+#a partir de acá, vienen todas las clases no referentes a personas interactuables, pero si objetos con
+#los que las personas pueden interactuar
+
 class Comedor:
-    ''''''
-    def __init__(self, caja, cocina, clientes, consolas, zona_relax):
-        self.caja = caja
-        self.cocina = cocina
-        self.clientes = clientes
-        self.consolas = consolas
-        self.zona_relax = zona_relax
+    '''Es el objeto de el lugar en si, donde almacenamos la caja donde están los cajeros,
+    la cocina donde están los cocinceros, a los clientes que vienen a interactuar, las consolas y los sofás'''
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.caja = []          #array de cajeros
+        self.cocina = []        #array de cocineros
+        self.clientes = []      #array de clientes
+        self.consolas = []      #array de consolas
+        self.zona_relax = []    #array de sofás
+
+    def agregar_trabajador(self, persona):
+        if type(persona) == Cajero:
+            self.caja.append(persona)
+        elif type(persona) == Cajero:
+            self.caja.append(persona)
+
+    def agregar_cliente(self, cliente):
+        self.clientes.append(cliente)
 
     #implmementar la creacion del mapa
     def recorrer(self):
@@ -112,7 +158,7 @@ class Comedor:
 
 
 class Sofa:
-    ''''''
+    '''Objeto con el que los objetos que heredan de "Accionable" pueden descansar'''
     def __init__(self, color, nivel_comodidad):
         self.color = color
         self.nivel_comodidad = nivel_comodidad
@@ -127,16 +173,33 @@ class Sofa:
             self.disponible = False
 
 class Consola:
-    ''''''
+    '''Las consolas con las que los clientes/cajeros pueden jugar, cambian su disponibilidad si alguno las está usando, cambian los estados de las personas'''
     def __init__(self, nombre):
         self.nombre = nombre
         self.disponible = True
 
     def jugar(self):
-        pass
+        if self.disponible == False:
+            print(f'La consola{self.nombre} está ocupada')
+        else:
+            self.disponible = False
 
 class Comida:
+    '''Las comidas cambian el estado de hambre de los clientes'''
     def __init__(self, nombre, nivel_sabor, nivel_complejidad):
         self.nombre = nombre
         self.nivel_sabor = nivel_sabor
         self.nivel_complejidad = nivel_complejidad
+
+
+class Mesa:
+    '''Las mesas son utilizadas por los clientes cuando comen, cambian su disponibilidad cuando algún cliente las usa'''
+    def __init__(self, numero_mesa):
+        self.numero_mesa = numero_mesa
+        self.disponible = True
+
+    def sentarse(self):
+        if(self.disponible == False):
+            print('la mesa está ocupada')
+        else:
+            print(f'El cliente está sentado en la mesa {self.numero_mesa}')
